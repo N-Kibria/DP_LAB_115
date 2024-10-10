fc96c1fe8ae2c2ff3317bf645397f39e49d638fd
N-Kibria <127234607+N-Kibria@users.noreply.github.com>
2024-10-10 22:58:17 +0600

    Update whippedCream.java

--- Lab_4/src/whippedCream.java
+++ Lab_4/src/whippedCream.java
@@ -1,10 +1,22 @@
-public class whippedCream implements IAddCondiment{
+public class whippedCream extends AddCondiment{
+
     @Override
     public Beverages addCondiment(Beverages b) {
 
-            //logic
+
             System.out.println("added Whipped cream");
+            System.out.println("Whipped Cream"+" :"+ Creamcount);
 
         return b;
     }
+
+    @Override
+    public double additionalcost() {
+        double  cost =.60*Creamcount;
+        Creamcount++;
+
+        return cost ;
+
+    }
+
 }
