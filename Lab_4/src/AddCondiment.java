6a85f0c9cba0a6f04e6dd53714fea9c820dee60d
N-Kibria <127234607+N-Kibria@users.noreply.github.com>
2024-10-10 22:55:22 +0600

    Update and rename IAddCondiment.java to AddCondiment.java

---
+++ Lab_4/src/AddCondiment.java
@@ -0,0 +1,8 @@
+public abstract class AddCondiment {
+    public  abstract Beverages addCondiment(Beverages b);
+    int milkcount=1;
+    int Creamcount=1;
+    int Sugercount=1;
+
+    public abstract double additionalcost();
+}
--- Lab_4/src/IAddCondiment.java
+++
@@ -1,6 +0,0 @@
-public interface IAddCondiment {
-    public Beverages addCondiment(Beverages b);
-
-
-
-}
