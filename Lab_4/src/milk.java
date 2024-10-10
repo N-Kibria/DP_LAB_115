86b4138008792c3662be95af8bafdea2d745daed
N-Kibria <127234607+N-Kibria@users.noreply.github.com>
2024-10-10 22:58:01 +0600

    Update milk.java

--- Lab_4/src/milk.java
+++ Lab_4/src/milk.java
@@ -1,10 +1,23 @@
-public class milk implements IAddCondiment{
+public class milk extends AddCondiment{
+
+
+
     @Override
     public Beverages addCondiment(Beverages b)
     {
-        //logic
+
         System.out.println("added milk");
+
+        System.out.println("milk"+" :"+ milkcount);
         return b;
+    }
+
+    @Override
+    public double additionalcost() {
+         double  cost =.50*milkcount;
+         milkcount++;
+
+        return cost ;
     }
 
 
