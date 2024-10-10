9ee74b168e158a1a17e0a53b7239248ef8c9a6b2
N-Kibria <127234607+N-Kibria@users.noreply.github.com>
2024-10-10 22:57:22 +0600

    Update Factory.java

--- Lab_4/src/Factory.java
+++ Lab_4/src/Factory.java
@@ -1,28 +1,39 @@
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Factory {
 
-    int c;
-    Map<Integer, IAddCondiment> dict = new HashMap<>();
-
-
+    Map<Integer, AddCondiment> condiments = new HashMap<>();
+    Map<Integer, Beverages> beverages = new HashMap<>();
 
     {
-        dict.put(1, new milk());
-        dict.put(2, new whippedCream());
-        dict.put(3, new whiteSugar());
+
+        condiments.put(1, new milk());
+        condiments.put(2, new whippedCream());
+        condiments.put(3, new whiteSugar());
+
+
+        beverages.put(1, new Espresso());
+        beverages.put(2, new Latte());
+        beverages.put(3, new Capuccino());
     }
 
-
+    public Beverages createBeverage(int choice) {
+        return beverages.get(choice);
+    }
+    public AddCondiment Addcondiment(int choice) {
+        return condiments.get(choice);
+    }
     public Beverages addCondiment(Beverages beverage, int choice) {
-        IAddCondiment condiment = dict.get(choice);
-        c++;
+        AddCondiment condiment = condiments.get(choice);
 
         if (condiment != null) {
             return condiment.addCondiment(beverage);
         }
         return beverage;
+    }
+
+    public double cost(Beverages beverage,AddCondiment condiment){
+        return beverage.totalcost(condiment);
     }
 }
